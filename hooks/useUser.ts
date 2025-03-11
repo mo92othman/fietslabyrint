ff63f3f2c208c696c93ca0c75f3557d7b277d000
mo92othman <mo92othman@gmail.com>
2025-03-11 22:23:17 +0100

    added tanstack and wrapped the app with provider

---
+++ hooks/useUser.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchUserData } from '@/lib/api';
+
+export const useUser = (username: string) => {
+  return useQuery({
+    queryKey: ['user'],
+    queryFn: () => fetchUserData(username),
+    retry: false,
+  });
+};
---
+++ providers/TanstackProvider.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+// import { queryClient } from './react-query';
+
+// const queryClient = new QueryClient();
+
+interface TanstackProviderProps {
+  children: React.ReactNode;
+}
+
+export const TanstackProvider = ({ children }: TanstackProviderProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
