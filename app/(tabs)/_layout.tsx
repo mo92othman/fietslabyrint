f326fef922939d66a95e32a6c69581ce75853250
mo92othman <mo92othman@gmail.com>
2025-03-12 11:26:04 +0100

    added tamagui library and wrapped the root layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -36,6 +36,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
